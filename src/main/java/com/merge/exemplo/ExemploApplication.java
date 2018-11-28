5f5dcd3165352f3fd3e42dc48b4ac21a1d5194cd
Adriano <adriano.gomes@azi.com.br>
2018-11-28 14:46:43 -0300

    git commit adriano

--- src/main/java/com/merge/exemplo/ExemploApplication.java
+++ src/main/java/com/merge/exemplo/ExemploApplication.java
@@ -8,5 +8,6 @@ public class ExemploApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExemploApplication.class, args);
+		System.out.println("Git commit adriano");
 	}
 }
